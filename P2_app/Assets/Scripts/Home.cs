b9223da8f1487675dd9fd9d252eaddbef1fc1640
LeoLetsPlay3rd <113899208+LeoLetsPlay3rd@users.noreply.github.com>
2023-04-20 12:21:14 +0200

    TextSavedInput

---
+++ P2_app/Assets/Scripts/Home.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Home : MonoBehaviour
+{
+    public TextMeshProUGUI display_challenge_name;
+
+    public void Awake()
+    {
+        display_challenge_name.text = Goals_Fitness.goalsFitnessScene.challenge_name;
+    }
+}
